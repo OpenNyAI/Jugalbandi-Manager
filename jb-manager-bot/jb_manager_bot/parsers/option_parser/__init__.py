9d3d2ee375fae37285b63dcfc48d9ddec7db388e
Shrey Pandey <shreypandey1509@gmail.com>
2024-02-29 11:04:21 +0530

    Added LLM manager

--- jb-manager-bot/jb_manager_bot/parsers/option_parser/__init__.py
+++ jb-manager-bot/jb_manager_bot/parsers/option_parser/__init__.py
@@ -2,24 +2,13 @@ import os
 import json
 from pathlib import Path
 
-from openai import OpenAI
-from jb_manager_bot.parsers.utils import llm, sm, am, um
-
+from jb_manager_bot.parsers.utils import LLMManager
 
 class OptionParser:
     """Parse the user input and return the most appropriate option ID based on the user's response."""
 
     with open(os.path.join(Path(__file__).parent, "prompt.txt"), "r", encoding="utf-8") as f:
         system_prompt = f.read()
-
-    openai_client = None
-
-    @classmethod
-    def get_client(cls, api_key):
-        """Return the OpenAI client."""
-        if cls.openai_client is None:
-            cls.openai_client = OpenAI(api_key=api_key)
-        return cls.openai_client
 
     @classmethod
     def parse(cls, api_key, user_task, options, user_input):
@@ -28,16 +17,16 @@ class OptionParser:
             if "id" not in option and not hasattr(option, "id"):
                 raise ValueError("Option ID is required")
 
-        result = llm(
-            client=cls.get_client(api_key=api_key),
+        result = LLMManager.llm(
+            api_key=api_key,
             messages=[
-                sm(cls.system_prompt),
-                am(
+                LLMManager.sm(cls.system_prompt),
+                LLMManager.am(
                     f"""User Task- {user_task}
     Options:
     - {options}"""
                 ),
-                um(user_input),
+                LLMManager.um(user_input),
             ],
             response_format={"type": "json_object"},
             model="gpt-3.5-turbo",
--- jb-manager-bot/jb_manager_bot/parsers/utils.py
+++ jb-manager-bot/jb_manager_bot/parsers/utils.py
@@ -1,68 +1,85 @@
 import json
-import openai
+from openai import OpenAI
+
+class LLMManager:
+    """Language Model Manager for OpenAI's GPT."""
+
+    client: OpenAI = None
+
+    @classmethod
+    def get_client(cls, api_key):
+        """Return the OpenAI client."""
+        if cls.client is None:
+            cls.client = OpenAI(api_key=api_key)
+        return cls.client
+
+    @classmethod
+    def llm(cls, api_key, messages, **kwargs):
+        """Use the OpenAI Language Model API to generate a response based on the given messages."""
+        kwargs["model"] = kwargs.get("model")
+        kwargs["messages"] = messages
+        args = {
+            k: v
+            for k, v in kwargs.items()
+            if k
+            in ["model", "messages", "temperature", "tools", "stream", "response_format"]
+        }
+
+        if args.get("temperature", None) is None:
+            args["temperature"] = 1e-6
+
+        client = cls.get_client(api_key)
+        completions = client.chat.completions.create(**args)
+
+        if args.get("stream", False):
+            full_response = ""
+            for chunk in completions:
+                for choice in chunk.choices:
+                    if choice.finish_reason == "stop":
+                        break
+                    if choice.delta.content is not None:
+                        kwargs["callback"](choice.delta.content)
+                        full_response += choice.delta.content
+            return full_response
+        elif args.get("tools", None) is None:
+            return completions.choices[0].message.content
+        else:
+            if completions.choices[0].message.tool_calls is None:
+                return {"function": None, "message": completions.choices[0].message.content}
+            function = completions.choices[0].message.tool_calls[0].function
+            return {"function": function.name, "arguments": json.loads(function.arguments)}
 
 
-def llm(client: openai.OpenAI, messages, **kwargs):
-    """Use the OpenAI Language Model API to generate a response based on the given messages."""
-    kwargs["model"] = kwargs.get("model")
-    kwargs["messages"] = messages
-    args = {
-        k: v
-        for k, v in kwargs.items()
-        if k
-        in ["model", "messages", "temperature", "tools", "stream", "response_format"]
-    }
-
-    if args.get("temperature", None) is None:
-        args["temperature"] = 1e-6
-
-    completions = client.chat.completions.create(**args)
-
-    if args.get("stream", False):
-        full_response = ""
-        for chunk in completions:
-            for choice in chunk.choices:
-                if choice.finish_reason == "stop":
-                    break
-                if choice.delta.content is not None:
-                    kwargs["callback"](choice.delta.content)
-                    full_response += choice.delta.content
-        return full_response
-    elif args.get("tools", None) is None:
-        return completions.choices[0].message.content
-    else:
-        if completions.choices[0].message.tool_calls is None:
-            return {"function": None, "message": completions.choices[0].message.content}
-        function = completions.choices[0].message.tool_calls[0].function
-        return {"function": function.name, "arguments": json.loads(function.arguments)}
+    @classmethod
+    def sm(cls, prompt):
+        """System message"""
+        return {"role": "system", "content": prompt}
 
 
-def sm(prompt):
-    """System message"""
-    return {"role": "system", "content": prompt}
+    @classmethod
+    def um(cls, prompt):
+        """User message"""
+        return {"role": "user", "content": prompt}
 
 
-def um(prompt):
-    """User message"""
-    return {"role": "user", "content": prompt}
+    @classmethod
+    def am(cls, prompt):
+        """Assistant message"""
+        return {"role": "assistant", "content": prompt}
 
 
-def am(prompt):
-    """Assistant message"""
-    return {"role": "assistant", "content": prompt}
-
-
-def fn(name, description, params, required):
-    """Function message"""
-    return {
-        "type": "function",
-        "function": {
-            "name": name,
-            "description": description,
-            "parameters": {
-                "type": "object",
-                "properties": {k: v for k, v in params.items()},
-                "required": required,
+    @classmethod
+    def fn(cls, name, description, params, required):
+        """Function message"""
+        return {
+            "type": "function",
+            "function": {
+                "name": name,
+                "description": description,
+                "parameters": {
+                    "type": "object",
+                    "properties": params,
+                    "required": required,
+                },
             },
-        },
-    }
+        }
