7727e47792ee6df59964d94695e805c648c3cf82
Shrey Pandey <shreypandey1509@gmail.com>
2024-05-22 17:25:55 +0530

    Added container ID fetch script

--- scripts/send-message.sh
+++ scripts/send-message.sh
@@ -3,5 +3,13 @@
 topic=$1
 message=$2
 
-echo $message | docker exec -i jb-manager-kafka-1 kafka-console-producer.sh --bootstrap-server localhost:9092 --topic $topic
+# Get the container ID for the service
+CONTAINER_ID=$(./scripts/get-container-id.sh kafka)
+
+# Check for error
+if [ $? -ne 0 ]; then
+    exit 1
+fi
+
+echo $message | docker exec -i $CONTAINER_ID kafka-console-producer.sh --bootstrap-server localhost:9092 --topic $topic
 echo "Sent message to $topic"
