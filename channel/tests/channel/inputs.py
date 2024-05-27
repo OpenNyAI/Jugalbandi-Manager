cd0869fc171c70cfc797e97191dddb551a58d28c
Sameer Segal <sameersegal@microsoft.com>
2024-05-27 12:55:18 +0530

    Discussing tests for channel service

---
+++ channel/tests/channel/inputs.py
@@ -0,0 +1,18 @@
+from unittest import TestCase
+from channel.main import 
+
+class TestChannelInputs(TestCase):
+
+    def test_channel_inputs(self):
+        self.assertTrue(True)
+
+
+# Input Message
+    # - channel, blob etc
+    # bot_input - channel 
+        -- a) update_message()
+        -- b) received_message
+    
+    # - crud
+# Output Message
+# Crud - depends on the DB
