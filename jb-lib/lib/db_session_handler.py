ba8c80971e44ffb5705f30173cfe14dcb2947cc2
Shrey Pandey <shreypandey1509@gmail.com>
2024-06-24 17:24:18 +0530

    Channel refactor and test cases (#106)
    * added test cases
    
    * Test cases added for outgoing messgage
    
    * Added channel test cases

---
+++ jb-lib/lib/db_session_handler.py
@@ -0,0 +1,50 @@
+import os
+import sqlalchemy
+from sqlalchemy.ext.asyncio import async_sessionmaker, create_async_engine
+from sqlalchemy.ext.declarative import declarative_base
+
+
+class DBSessionHandler:
+    __async_session__ = None
+    __db_name__ = os.getenv("POSTGRES_DATABASE_NAME")
+    __db_user__ = os.getenv("POSTGRES_DATABASE_USERNAME")
+    __db_password__ = os.getenv("POSTGRES_DATABASE_PASSWORD")
+    __db_host__ = os.getenv("POSTGRES_DATABASE_HOST")
+    __db_port__ = os.getenv("POSTGRES_DATABASE_PORT")
+
+    # Construct the SQLAlchemy connection URL using URL class
+    db_url = f"postgresql+asyncpg://{__db_user__}:{__db_password__}@{__db_host__}:{__db_port__}/{__db_name__}"
+
+    @classmethod
+    def __initialise_async_session__(cls):
+        if cls.__async_session__ is None:
+            engine = create_async_engine(
+                cls.db_url,
+                future=True,
+                echo=False,
+                max_overflow=50,
+                pool_size=25,
+                pool_timeout=60,
+                pool_recycle=3600,
+                pool_pre_ping=True,
+            )
+            async_session = async_sessionmaker(
+                bind=engine, autocommit=False, autoflush=False, expire_on_commit=False
+            )
+            metadata = sqlalchemy.MetaData(
+                naming_convention={
+                    "ix": "ix_%(column_0_label)s",
+                    "uq": "uq_%(table_name)s_%(column_0_name)s",
+                    "ck": "ck_%(table_name)s_`%(constraint_name)s`",
+                    "fk": "fk_%(table_name)s_%(column_0_name)s_%(referred_table_name)s",
+                    "pk": "pk_%(table_name)s",
+                }
+            )
+            Base = declarative_base(metadata=metadata)
+            cls.__async_session__ = async_session
+
+    @classmethod
+    def get_async_session(cls):
+        if cls.__async_session__ is None:
+            cls.__initialise_async_session__()
+        return cls.__async_session__()
