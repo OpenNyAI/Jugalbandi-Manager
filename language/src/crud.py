2a299d1a3f88445c590f5d3003f271fe49bb3b9c
Shrey Pandey <shreypandey1509@gmail.com>
2024-07-05 12:11:43 +0530

    DB session handler (#127)
    * Applied black formatting
    
    * Added black formatting
    
    * Formatted files
    
    * Updated gh and precommit hook
    
    * Updated language image
    
    * Updated language poetry lock file
    
    * Added sync db handler
    
    * Updated db session handler usgae
    
    * Deleted DB connection file

--- language/src/crud.py
+++ language/src/crud.py
@@ -1,12 +1,12 @@
 from sqlalchemy import select
 
-from lib.db_connection import async_session
+from lib.db_session_handler import DBSessionHandler
 from lib.models import JBMessage, JBTurn, JBUser, JBSession
 
 
 async def get_user_preferred_language(session_id: str):
     query = select(JBSession.pid).where(JBSession.id == session_id)
-    async with async_session() as session:
+    async with DBSessionHandler.get_async_session() as session:
         async with session.begin():
             result = await session.execute(query)
             pid = result.scalars().first()
@@ -14,9 +14,9 @@ async def get_user_preferred_language(session_id: str):
             return preferred_language
 
 
-async def get_user_preferred_language_by_pid(pid: str):        
+async def get_user_preferred_language_by_pid(pid: str):
     query = select(JBUser.language_preference).where(JBUser.id == pid)
-    async with async_session() as session:
+    async with DBSessionHandler.get_async_session() as session:
         async with session.begin():
             result = await session.execute(query)
             preferred_language = result.scalars().first()
@@ -25,15 +25,16 @@ async def get_user_preferred_language_by_pid(pid: str):
 
 async def get_message_media_information(msg_id: str):
     query = select(JBMessage).where(JBMessage.id == msg_id)
-    async with async_session() as session:
+    async with DBSessionHandler.get_async_session() as session:
         async with session.begin():
             result = await session.execute(query)
             message = result.scalars().first()
             return message
 
+
 async def get_turn_information(turn_id: str):
     query = select(JBTurn).where(JBTurn.id == turn_id)
-    async with async_session() as session:
+    async with DBSessionHandler.get_async_session() as session:
         async with session.begin():
             result = await session.execute(query)
             turn = result.scalars().first()
