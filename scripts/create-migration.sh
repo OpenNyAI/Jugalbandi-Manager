62a010e94dbcaec378effa44f7f81ab8ca8e7aca
kanak8278 <kanak8278@gmail.com>
2024-07-08 12:46:52 +0530

    install jb-lib, checking python version 3.11

--- scripts/create-migration.sh
+++ scripts/create-migration.sh
@@ -1,10 +1,15 @@
 #!/bin/bash
 
 msg=$1
-
+if ! python3 -c 'import sys; assert sys.version_info >= (3, 11)' 2>/dev/null; then
+    echo "Please use python3.11 or newer"
+    exit 1
+fi
 python3 -m venv .venv
 source .venv/bin/activate
 pip3 install alembic psycopg2-binary
+pip3 install -e ./jb-lib/
+
 
 # source ./set_jbhost.sh
 
