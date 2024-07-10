55e925517e9a934687ee8d67d59030304b716eab
Shrey Pandey <shreypandey1509@gmail.com>
2024-07-10 16:56:15 +0530

    Fixed create topic script (#137)

--- scripts/create-all-topics.sh
+++ scripts/create-all-topics.sh
@@ -12,8 +12,10 @@ done
 
 echo "Creating topics now"
 
-kafka-topics.sh --create --bootstrap-server localhost:9092 --topic channel --if-not-exists
-kafka-topics.sh --create --bootstrap-server localhost:9092 --topic language --if-not-exists
-kafka-topics.sh --create --bootstrap-server localhost:9092 --topic flow --if-not-exists
-kafka-topics.sh --create --bootstrap-server localhost:9092 --topic retriever --if-not-exists
-kafka-topics.sh --create --bootstrap-server localhost:9092 --topic indexer --if-not-exists
+kafka-topics.sh --create --bootstrap-server localhost:9092 --topic $KAFKA_FLOW_TOPIC --if-not-exists
+kafka-topics.sh --create --bootstrap-server localhost:9092 --topic $KAFKA_LANGUAGE_TOPIC --if-not-exists
+kafka-topics.sh --create --bootstrap-server localhost:9092 --topic $KAFKA_CHANNEL_TOPIC --if-not-exists
+kafka-topics.sh --create --bootstrap-server localhost:9092 --topic $KAFKA_RETRIEVER_TOPIC --if-not-exists
+kafka-topics.sh --create --bootstrap-server localhost:9092 --topic $KAFKA_INDEXER_TOPIC --if-not-exists
+
+kafka-topics.sh --bootstrap-server localhost:9092 --list
