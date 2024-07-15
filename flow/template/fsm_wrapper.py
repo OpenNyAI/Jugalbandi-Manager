70a03da32cd8ac98e93f950b2e34c33779958e33
Shrey Pandey <shreypandey1509@gmail.com>
2024-07-15 13:32:00 +0530

    Conversation Reset Added (#145)
    * Refactored v1 handlers
    
    * Added v2 routers and handlers
    
    * Added Tag to router
    
    * Incorporated data model changes in API, Channel, Language
    
    * Refactor Flow bot installation logic
    
    * Added flow test cases
    
    * Updated test case for bot install
    
    * Deleted data models
    
    * Added db migration
    
    * Updated fsm runner
    
    * Updated car wash fsm
    
    * Bumped version of jb manager bot'
    
    * Tested changes
    
    * Final migration
    
    * Added conversation reset
    
    * Removed unused curd operations
    
    * Removed unused crud operations

--- flow/template/fsm_wrapper.py
+++ flow/template/fsm_wrapper.py
@@ -4,7 +4,7 @@ import json
 from cryptography.fernet import Fernet
 import bot
 from jb_manager_bot import AbstractFSM
-from jb_manager_bot.data_models import FSMOutput, FSMInput
+from jb_manager_bot.data_models import FSMOutput, FSMInput, FSMIntent
 
 
 def decrypt_credentials(credentials: dict) -> dict:
@@ -32,12 +32,14 @@ fsm_input = runner_input.get("fsm_input")
 fsm_input = FSMInput.model_validate(fsm_input)
 jb_bot: AbstractFSM = getattr(bot, bot_name)
 
-new_state = jb_bot.run_machine(
-    send_message=callback_function,
-    user_input=fsm_input.user_input,
-    callback_input=fsm_input.callback_input,
-    state=fsm_state_dict,
-    credentials=decrypt_credentials(credentials),
-)
-
-print(json.dumps({"new_state": new_state}))
+if fsm_input.user_input and fsm_input.user_input.lower() == "hi":
+    callback_function(FSMOutput(intent=FSMIntent.CONVERSATION_RESET))
+else:
+    new_state = jb_bot.run_machine(
+        send_message=callback_function,
+        user_input=fsm_input.user_input,
+        callback_input=fsm_input.callback_input,
+        state=fsm_state_dict,
+        credentials=decrypt_credentials(credentials),
+    )
+    print(json.dumps({"new_state": new_state}))
