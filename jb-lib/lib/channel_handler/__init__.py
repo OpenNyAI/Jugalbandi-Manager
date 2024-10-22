92d78e0c267532588d7253f4f3a162ccb987445f
Karanraj <karanraj.m@thoughtworks.com>
2024-10-22 16:32:54 +0530

    Added custom channel code

--- jb-lib/lib/channel_handler/__init__.py
+++ jb-lib/lib/channel_handler/__init__.py
@@ -1,9 +1,12 @@
 from typing import Dict
-from .channel_handler import ChannelHandler, ChannelData, User
+
+from .channel_handler import ChannelHandler
+from .custom_channel_handler import CustomChannelHandler
 from .pinnacle_whatsapp_handler import PinnacleWhatsappHandler
 from .telegram_handler import TelegramHandler
 
 channel_map: Dict[str, type[ChannelHandler]] = {
     PinnacleWhatsappHandler.get_channel_name(): PinnacleWhatsappHandler,
     TelegramHandler.get_channel_name(): TelegramHandler,
+    CustomChannelHandler.get_channel_name(): CustomChannelHandler,
 }
